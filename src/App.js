eda2db0041ab8fad524ae1c355f86884553c3699
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-04 09:54:17 +0100

    update Hero content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div ></div>
+    <div >
+      <Header/>
+    </div>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,17 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='banner'>
+       <div className='banner_content'>
+            <div className='container'>
+                <h3>Efficient and Automated</h3>
+                <h1>FUND INVEST</h1>
+                <p>
+                    Unlock Financial Prosperity: Empower Your Future with Fund Invest.
+                    We help you cultivate wealth with precision. Financial Success Begins
+                    Here.
+                </p>
+            </div>
+       </div> 
       
     </div>
   )
