11b8eb5514e21d87571167c856e405122b33cf96
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-04 10:38:46 +0100

    upload fund invest logo

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='navbar_text'>
+            <div className='logo'>
+
+            </div>
+
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
