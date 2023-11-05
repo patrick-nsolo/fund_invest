bbf566a3239a2c3abaa9860eb8bc57de13971876
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-05 20:32:22 +0100

    update phone section

--- src/Components/PhoneSection/Phonesection.js
+++ src/Components/PhoneSection/Phonesection.js
@@ -4,19 +4,26 @@ import './Phonesection.css'
 const Phonesection = () => {
   return (
     <div className='phone-section'>
-        <div className='container'>
+      <div className='phone-section-container'>
+        <div className='row'>
+          <div className='col-6'>
             <h3>About Us</h3>
-            <h1>WELCOME TO FUND INVEST</h1>
-            <p>There’s no secret to long-term wealth, but if there were it would be this:
-            <span>automated index investing.</span></p>
-            <p>
-            Even with the inevitable ups and downs of the market, our expert-built, 
-            globally-diversified Automated Investing Account makes it easy to start 
-            building long-term wealth by managing your risk, maximizing returns, 
-            and minimizing taxes.
-            </p>
+              <h1>WELCOME TO FUND INVEST</h1>
+              <p>There’s no secret to long-term wealth, but if there were it would be this:
+              <span>automated index investing.</span>
+              </p>
+              <p>
+              Even with the inevitable ups and downs of the market, our expert-built, 
+              globally-diversified Automated Investing Account makes it easy to start 
+              building long-term wealth by managing your risk, maximizing returns, 
+              and minimizing taxes.
+              </p>
+          </div>
+          <div className='col-6'>
+
+          </div>
         </div>
-      
+      </div>  
     </div>
   )
 }
