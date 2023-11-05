d0cfcae731b9f681cb8d2cc1c1a634f1c3b731b3
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-05 20:09:22 +0100

    PhoneSection update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from '../src/Components/Header/Header';
-
+import Phonesection from './Components/PhoneSection/Phonesection';
 
 function App() {
   return (
     <div >
       <Header/>
+      <Phonesection/>
     </div>
   );
 }
