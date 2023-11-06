c3248c9ecc17cd4e71dece92ed854893e6c8d2b0
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-11-06 16:02:15 +0100

    Create risk tolerance section

---
+++ src/Components/riskTolerance/riskTolerance.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const riskTolerance = () => {
+  return (
+    <div>
+      <h2>Select your Risk Tolerance</h2>
+      <button>Low</button>
+      <button>Medium</button>
+      <button>High</button>  
+    </div>
+  )
+}
+
+export default riskTolerance
+
